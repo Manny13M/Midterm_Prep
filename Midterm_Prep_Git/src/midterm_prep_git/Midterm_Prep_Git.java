d6ed88677c31deea969dcd432f4a0b1ec7fec63c
Manuel <martmanu@sheridancollege.ca>
2022-06-23 07:09:53 -0400

    First comment 7:09am, Manuel Martinez

---
+++ Midterm_Prep_Git/src/midterm_prep_git/Midterm_Prep_Git.java
@@ -0,0 +1,20 @@
+
+
+package midterm_prep_git;
+
+/**
+ *
+ * @author Manuel Martinez
+ */
+public class Midterm_Prep_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+
+//If you see this, push was successful
