12866484ccf82990b54d6c7e5609f642248b31c8
Manuel <martmanu@sheridancollege.ca>
2022-06-23 07:32:40 -0400

    Created branch tag 7:32am, Manuel Martinez

---
+++ Midterm_Prep_Git/src/midterm_prep_git/Manuels_Branch.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midterm_prep_git;
+
+/**
+ *
+ * @author manuelmartinez
+ */
+public class Manuels_Branch {
+    
+}
